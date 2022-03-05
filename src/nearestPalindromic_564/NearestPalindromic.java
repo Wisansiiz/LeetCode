51a501c99b0821d5a8dc25263e820148b2a36eca
Wisansiiz <Wisansiiz@DESKTOP-VBES6HU>
2022-03-05 12:15:54 +0800

    DailyPractice

--- src/nearestPalindromic_564/NearestPalindromic.java
+++ src/nearestPalindromic_564/NearestPalindromic.java
@@ -1,54 +1,76 @@
 package nearestPalindromic_564;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class NearestPalindromic {
+/*
+ * 给定一个表示整数的字符串 n ，
+ * 返回与它最近的回文整数（不包括自身）。
+ * 如果不止一个，返回较小的那个。
 
+“最近的”定义为两个整数差的绝对值最小。
+ * */
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-		String n = "99321";
-		System.out.print(new Solution().nearestPalindromic(n));
+		String n = "100";
+		System.out.print(new Solution2().nearestPalindromic(n));
 	}
 
 }
-class Solution {
+class Solution2 {
     public String nearestPalindromic(String n) {
-        long selfNumber = Long.parseLong(n), ans = -1;
-        List<Long> candidates = getCandidates(n);
-        for (long candidate : candidates) {
-            if (candidate != selfNumber) {
-                if (ans == -1 ||
-                    Math.abs(candidate - selfNumber) < Math.abs(ans - selfNumber) ||
-                    Math.abs(candidate - selfNumber) == Math.abs(ans - selfNumber) && candidate < ans) {
-                    ans = candidate;
-                }
-            }
-        }
-        return Long.toString(ans);
+    	StringBuilder sb = new StringBuilder();
+    	String ans = near(sb, n), res = "";
+    	long num = Long.valueOf(n), min = Long.MAX_VALUE;
+    	if(n.length() == 1 && num != 0)
+    		return (num - 1)+"";
+    	if(num == Math.pow(10, n.length()-1))
+    		return (num - 1)+"";
+    	if(!ans.equals(n))
+    	{
+    		long dif = Math.abs(Long.valueOf(ans) - num);
+    		String num1 = near(sb, (long) (num - Math.pow(10, n.length()/2))+"");
+    		long dif1 = Math.abs(Long.valueOf(num) - Long.valueOf(num1));
+    		String num2 = near(sb, (long) (num + Math.pow(10, n.length()/2))+""); 
+    		long dif2 = Math.abs(Long.valueOf(num) - Long.valueOf(num2));
+    		long tem = dif < dif1 ? dif:dif1;
+    		String tem1 = dif < dif1 ? ans : num1;
+    		long tem2 = tem < dif2 ? tem : dif2;
+    		ans = tem <= tem2 ? tem1 : num2;
+    	}
+    	else
+    	{
+    		for(long i = (long) (num - Math.pow(10, n.length()/2)) - 1; i < (long) (num + Math.pow(10, n.length()/2)); i++)
+    		{
+    			if((i+"").length() == 1 && (i + 1 + "").length() == 2)
+    			{
+    				ans = i+"";
+    				break;
+    			}
+    			ans = near(sb, i+"");
+    			if(!ans.equals(n))
+    			{
+    				if(Math.abs(Long.valueOf(ans) - num) < min)
+    				{
+    					min = Math.abs(Long.valueOf(ans) - num);
+    					res = ans;
+    				}
+    			}
+    			ans = res;
+    		}
+    	}
+    	return ans;
     }
-
-    public List<Long> getCandidates(String n) {
-        int len = n.length();
-        List<Long> candidates = new ArrayList<Long>() {/**
-			 * 
-			 */
-			private static final long serialVersionUID = 773668887383326656L;
-
-		{
-            add((long) Math.pow(10, len - 1) - 1);
-            add((long) Math.pow(10, len) + 1);
-        }};
-        long selfPrefix = Long.parseLong(n.substring(0, (len + 1) / 2));
-        for (long i = selfPrefix - 1; i <= selfPrefix + 1; i++) {
-            StringBuffer sb = new StringBuffer();
-            String prefix = String.valueOf(i);
-            sb.append(prefix);
-            StringBuffer suffix = new StringBuffer(prefix).reverse();
-            sb.append(suffix.substring(len & 1));
-            String candidate = sb.toString();
-            candidates.add(Long.parseLong(candidate));
-        }
-        return candidates;
-    }
+    String near(StringBuilder sb, String n)
+	{
+		int i = 0;
+		long len = n.length();
+		for (; i < len / 2; i++) 
+			sb.append(n.charAt(i));
+		if (len % 2 == 0)
+			i--;
+		for (; i >= 0; i--) 
+			sb.append(n.charAt(i));
+		String s = sb.toString();
+		sb.delete(0, sb.length());
+		return s;
+	}
 }
